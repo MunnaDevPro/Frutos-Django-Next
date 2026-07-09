5b02cbe4b33215417119de21af6f85f43db79c76
Md Munna <munna.codeexpert@gmail.com>
2026-07-09 18:24:37 +0600

    updateddddd

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -91,7 +91,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -804,6 +804,4 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
-
-
+DEFAULT_FROM_EMAIL = f"El Arbol Support <{os.environ.get('EMAIL_HOST_USER')}>"
